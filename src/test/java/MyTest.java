d31328d2885c2bba3466f37022d0f1f02bf3caa2
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-20 11:36:22 +0300

    codewars-make-uppercase

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+        public void testSomething() {
+            Upper u = new Upper();
+            assertEquals("HELLO",u.MakeUpperCase("hello"));
+        }
 
+        private static class Upper {
+            public String MakeUpperCase(String str) {
+                return str.toUpperCase();
+            }
+        }
     }
-}
+
